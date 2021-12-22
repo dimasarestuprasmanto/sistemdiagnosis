dd23ed832752f69f0a51119ccc63ca1020d8342f
AriSmrd7 <arismrd13@gmail.com>
2021-12-22 15:24:46 +0700

    update

---
+++ db_sistempakar.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Dec 22, 2021 at 09:24 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_sistempakar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_rule`
+--
+
+CREATE TABLE `data_rule` (
+  `id_data_rule` int(11) NOT NULL,
+  `id_value` int(11) NOT NULL,
+  `id_rule` int(11) NOT NULL,
+  `val_konklusi` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konsultasi`
+--
+
+CREATE TABLE `konsultasi` (
+  `id_konsultasi` int(11) NOT NULL,
+  `id_solusi` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `hasil_k` varchar(200) NOT NULL,
+  `tanggal_k` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parameter`
+--
+
+CREATE TABLE `parameter` (
+  `id_parameter` int(10) NOT NULL,
+  `nama_parameter` varchar(200) NOT NULL,
+  `pertanyaan` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rule`
+--
+
+CREATE TABLE `rule` (
+  `id_rule` int(11) NOT NULL,
+  `nama_set` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `solusi`
+--
+
+CREATE TABLE `solusi` (
+  `id_solusi` int(11) NOT NULL,
+  `id_value` int(11) NOT NULL,
+  `opt` varchar(50) NOT NULL,
+  `gejala` varchar(200) NOT NULL,
+  `solusi` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `level` varchar(10) NOT NULL,
+  `alamat` varchar(300) NOT NULL,
+  `no_telp` varchar(20) NOT NULL,
+  `tanggal_registrasi` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama`, `username`, `password`, `level`, `alamat`, `no_telp`, `tanggal_registrasi`) VALUES
+(1, 'Ari Sumardi', 'admin', '21232f297a57a5a743894a0e4a801fc3', '1', 'Tangerang', '085863727216', '2021-12-22 06:42:10'),
+(2, 'Dimas Arestu', 'user', 'ee11cbb19052e40b07aac0ca060c23ee', '2', 'Pekanbaru', '087223727211', '2021-12-22 06:42:54');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `value`
+--
+
+CREATE TABLE `value` (
+  `id_value` int(11) NOT NULL,
+  `id_parameter` int(11) NOT NULL,
+  `val_value` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_rule`
+--
+ALTER TABLE `data_rule`
+  ADD PRIMARY KEY (`id_data_rule`),
+  ADD KEY `id_value` (`id_value`),
+  ADD KEY `id_rule` (`id_rule`);
+
+--
+-- Indexes for table `konsultasi`
+--
+ALTER TABLE `konsultasi`
+  ADD PRIMARY KEY (`id_konsultasi`),
+  ADD KEY `id_solusi` (`id_solusi`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `parameter`
+--
+ALTER TABLE `parameter`
+  ADD PRIMARY KEY (`id_parameter`);
+
+--
+-- Indexes for table `rule`
+--
+ALTER TABLE `rule`
+  ADD PRIMARY KEY (`id_rule`);
+
+--
+-- Indexes for table `solusi`
+--
+ALTER TABLE `solusi`
+  ADD PRIMARY KEY (`id_solusi`),
+  ADD KEY `id_value` (`id_value`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indexes for table `value`
+--
+ALTER TABLE `value`
+  ADD PRIMARY KEY (`id_value`),
+  ADD KEY `id_parameter` (`id_parameter`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_rule`
+--
+ALTER TABLE `data_rule`
+  MODIFY `id_data_rule` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `konsultasi`
+--
+ALTER TABLE `konsultasi`
+  MODIFY `id_konsultasi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `parameter`
+--
+ALTER TABLE `parameter`
+  MODIFY `id_parameter` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rule`
+--
+ALTER TABLE `rule`
+  MODIFY `id_rule` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `solusi`
+--
+ALTER TABLE `solusi`
+  MODIFY `id_solusi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `value`
+--
+ALTER TABLE `value`
+  MODIFY `id_value` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `data_rule`
+--
+ALTER TABLE `data_rule`
+  ADD CONSTRAINT `data_rule_ibfk_1` FOREIGN KEY (`id_value`) REFERENCES `value` (`id_value`),
+  ADD CONSTRAINT `data_rule_ibfk_2` FOREIGN KEY (`id_rule`) REFERENCES `rule` (`id_rule`);
+
+--
+-- Constraints for table `konsultasi`
+--
+ALTER TABLE `konsultasi`
+  ADD CONSTRAINT `konsultasi_ibfk_1` FOREIGN KEY (`id_solusi`) REFERENCES `solusi` (`id_solusi`),
+  ADD CONSTRAINT `konsultasi_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `solusi`
+--
+ALTER TABLE `solusi`
+  ADD CONSTRAINT `solusi_ibfk_1` FOREIGN KEY (`id_value`) REFERENCES `value` (`id_value`);
+
+--
+-- Constraints for table `value`
+--
+ALTER TABLE `value`
+  ADD CONSTRAINT `value_ibfk_1` FOREIGN KEY (`id_parameter`) REFERENCES `parameter` (`id_parameter`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
