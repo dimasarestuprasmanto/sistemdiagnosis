eda17156e19616034af9dc0a875ec931092a3aa6
AriSmrd7 <arismrd13@gmail.com>
2022-01-13 18:32:38 +0700

    update

--- application/views/user/detail_penyakit_view.php
+++ application/views/user/detail_penyakit_view.php
@@ -3,16 +3,28 @@
         <div class="col-12 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
-                    <h2 class="">Penyakit : <?= $data['name']; ?></h2>
-                    <img class="img-fluid" src="<?= base_url('assets/images/penyakit/' . $data['image']) ?>" alt="Responsive image" />
+                    <h3 class="card-title">Nama Penyakit : <strong class="text-info"><?= $data['name']; ?></strong></h3>
 
-                    <h2 class="mt-5">Deskripsi Penyakit</h2>
-                    <p><?= $data['description']; ?></p>
-                    <h2>Gejala Penyakit</h2>
-                    <p><?= $gejala['detail']; ?></p>
-                    <h2>Solusi Penyembuhan</h2>
-                    <p><?= $data['solution']; ?></p>
+                    <img class="img-fluid" src="<?= base_url('assets/images/penyakit/' . $data['image']) ?>" style="width: max-content; align-items:center;" />
+
+                    <h3 class="card-title mb-3 mt-5">Deskripsi Penyakit</h3>
+                    <blockquote class="blockquote blockquote-info">
+                        <p class="mb-0"><?= $data['description']; ?></p>
+                    </blockquote>
+
+                    <h3 class="card-title mb-3 mt-5">Gejala Penyakit</h3>
+                    <blockquote class="blockquote blockquote-info">
+                        <p class="mb-0"><?= $gejala['detail']; ?></p>
+                    </blockquote>
+
+                    <h3 class="card-title mb-3 mt-5">Solusi Penyembuhan</h3>
+                    <blockquote class="blockquote blockquote-info">
+                        <p class="mb-0"><?= $data['solution']; ?></p>
+                    </blockquote>
                 </div>
             </div>
+        </div>
+        <div class="mb-5 mt-3"">
+            <button onclick="history.back()" class="btn btn-md btn-block btn-success">Kembali</button>
         </div>
     </div>
--- application/views/user/info_penyakit_view.php
+++ application/views/user/info_penyakit_view.php
@@ -1,24 +1,23 @@
 <div class="main-panel">
     <div class="content-wrapper">
-
         <section class="py-5">
+        <h3 class="card-title">Daftar Penyakit</h3>
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <!-- item card -->
                     <?php foreach ($datapenyakit as $p) : ?>
                         <div class="col mb-5">
                             <div class="card">
                                 <!-- Product image-->
-                                <img class="card-img-top img-responsive" src="<?= base_url('assets/images/penyakit/' . $p->image) ?>" alt="..." width="150px" height="230px" />
+                                <img class="card-img-top img-responsive" src="<?= base_url('assets/images/penyakit/' . $p->image) ?>" alt="..." width="170px" height="230px" />
                                 <!-- Product details-->
                                 <div class="text-center">
                                     <!-- Product name-->
                                     <h6 class="fw-bolder">
-                                        <h5 class="truncate mt-1"><?= $p->name ?></h5>
+                                        <p class="truncate mt-1"><?= $p->name ?></p>
                                     </h6>
 
                                     <div class="card-footer pt-0 border-top-0 bg-transparent">
-                                        <a href="info/detail/<?= encrypt_url($p->id) ?>" class="btn btn-success">Detail</a>
+                                        <a href="info/detail/<?= encrypt_url($p->id) ?>" class="btn btn-sm btn-success">Detail</a>
                                     </div>
                                 </div>
                             </div>
